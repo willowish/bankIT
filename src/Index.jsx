1c65695278d8802da598682fc88d1ddf409ebaf0
mawierzb <mateusz.wierzbicki@nokia.com>
2017-05-19 22:16:13 +0200

    Webpack + react

---
+++ src/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Index extends React.Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (<div><h1>Sema!</h1></div>);
+	}
+}
---
+++ src/indexApp.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Index from './Index.jsx';
+const app = document.getElementById('app');
+
+ReactDOM.render(<Index />, app);
